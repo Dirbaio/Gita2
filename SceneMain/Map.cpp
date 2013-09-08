964956c7d8ea7acd02e303d2add4699ab948227a
Dirbaio <dirbaio@dirbaio.net>
2013-09-09 00:38:45 +0200

    Better generator

--- SceneMain/Map.cpp
+++ SceneMain/Map.cpp
@@ -18,18 +18,28 @@ struct Street
 		//roadway = 0 => calle peatonal :D
 };
 
+const int streetTypes[][2] = {
+	{2, 0},
+	{1, 2},
+	{1, 3},
+	{2, 4},
+};
 std::vector<Street> generateStreets(int size)
 {
 	std::vector<Street> v;
 
 	for(int i = 1; i < size; i++)
 	{
+		int t = rand()%4;
 		Street s;
 		s.pos = i;
-		s.sidewalk = randInt(0, 2);
-		s.roadway = randInt(0, 4);
-		v.push_back(s);
-		i += s.sidewalk*2+s.roadway+16;
+		s.sidewalk = streetTypes[t][0];
+		s.roadway = streetTypes[t][1];
+		i += s.sidewalk*2+s.roadway;
+		if(i < size-1)
+			v.push_back(s);
+
+		i += randInt(16, 20);
 	}
 	return v;
 }
@@ -40,35 +50,36 @@ Map::Map(SceneMain* scene) : GameObject(scene)
 	int sizey = 120;
 	tiles = std::vector<std::vector<Tile> > (sizex, std::vector<Tile>(sizey));
 
-	std::vector<Street> sx = generateStreets(sizex);
-	std::vector<Street> sy = generateStreets(sizey);
+	std::vector<Street> sx = generateStreets(sizey);
+	std::vector<Street> sy = generateStreets(sizex);
 
 	//Generate the map
 	for(int y = 0; y < sizey; y++)
 		for(int x = 0; x < sizex; x++)
 			tile(x, y).type = Map::Garden;
 
-	const int blockSizeMin = 4;
-	const int blockSizeMax = 7;
+	int xmin = sy[0].pos;
+	int xmax = sy[sy.size()-1].pos+sy[sy.size()-1].sidewalk*2+sy[sy.size()-1].roadway;
+	int ymin = sx[0].pos;
+	int ymax = sx[sx.size()-1].pos+sx[sx.size()-1].sidewalk*2+sx[sx.size()-1].roadway;
 
-	//Generate streets
-	for(int y = blockSizeMin; y+blockSizeMin < sizey; y++)
+	for(int i = 0; i < sx.size(); i++)
 	{
-		int start = randInt(0, sizex/2-10);
-		int end = randInt(sizex/2+10, sizex);
-		for(int x = start; x < end; x++)
-			tile(x, y).type = Map::Sidewalk;
-		y += randInt(blockSizeMin, blockSizeMax);
+		Street& s = sx[i];
+		int width = s.sidewalk*2+s.roadway;
+		for(int x = xmin; x < xmax; x++)
+			for(int y = 0; y < width; y++)
+				tile(x, s.pos+y).type = Map::Sidewalk;
+	}
+	for(int i = 0; i < sy.size(); i++)
+	{
+		Street& s = sy[i];
+		int width = s.sidewalk*2+s.roadway;
+		for(int x = ymin; x < ymax; x++)
+			for(int y = 0; y < width; y++)
+				tile(s.pos+y, x).type = Map::Sidewalk;
 	}
 
-	for(int x = blockSizeMin; x+blockSizeMin < sizex; x++)
-	{
-		int start = randInt(0, sizey/2-10);
-		int end = randInt(sizey/2+10, sizey);
-		for(int y = start; y < end; y++)
-			tile(x, y).type = Map::Sidewalk;
-		x += randInt(blockSizeMin, blockSizeMax);
-	}
 	srand(time(0));
 
 	//Generate the mesh
